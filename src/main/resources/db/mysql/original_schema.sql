da3ad364bfc9ad20869e29870e3810b400253807
theamistudy <the.amistudy@gmail.com>
2018-12-06 16:39:12 +0530

    Default profile changed.

--- src/main/resources/db/mysql/original_schema.sql
+++ src/main/resources/db/mysql/original_schema.sql
@@ -1,19 +1,27 @@
+CREATE DATABASE IF NOT EXISTS petclinic;
 
+ALTER DATABASE petclinic
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
 
-CREATE TABLE  vets (
+GRANT ALL PRIVILEGES ON petclinic.* TO pc@localhost IDENTIFIED BY 'pc';
+
+USE petclinic;
+
+CREATE TABLE IF NOT EXISTS vets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   INDEX(last_name)
 ) engine=InnoDB;
 
-CREATE TABLE  specialties (
+CREATE TABLE IF NOT EXISTS specialties (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(80),
   INDEX(name)
 ) engine=InnoDB;
 
-CREATE TABLE  vet_specialties (
+CREATE TABLE IF NOT EXISTS vet_specialties (
   vet_id INT(4) UNSIGNED NOT NULL,
   specialty_id INT(4) UNSIGNED NOT NULL,
   FOREIGN KEY (vet_id) REFERENCES vets(id),
@@ -21,13 +29,13 @@ CREATE TABLE  vet_specialties (
   UNIQUE (vet_id,specialty_id)
 ) engine=InnoDB;
 
-CREATE TABLE  types (
+CREATE TABLE IF NOT EXISTS types (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(80),
   INDEX(name)
 ) engine=InnoDB;
 
-CREATE TABLE  owners (
+CREATE TABLE IF NOT EXISTS owners (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
@@ -37,7 +45,7 @@ CREATE TABLE  owners (
   INDEX(last_name)
 ) engine=InnoDB;
 
-CREATE TABLE  pets (
+CREATE TABLE IF NOT EXISTS pets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30),
   birth_date DATE,
@@ -48,7 +56,7 @@ CREATE TABLE  pets (
   FOREIGN KEY (type_id) REFERENCES types(id)
 ) engine=InnoDB;
 
-CREATE TABLE  visits (
+CREATE TABLE IF NOT EXISTS visits (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   pet_id INT(4) UNSIGNED NOT NULL,
   visit_date DATE,
